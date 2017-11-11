5a90938c85499354f28f036c92f4219dba9dc360
Danslans <danslans77@gmail.com>
2017-11-11 17:31:49 -0500

    correccion de css

--- js/DbScript.js
+++ js/DbScript.js
@@ -1,5 +1,22 @@
-function CreateDb(){
-	var db=initDb();
+class Persons {
+
+constructor(){
+}	
+
+ CreateDb(){
+	var dbIndex=window.indexedDB.open("mydb",1);
+		dbIndex.onupgradeneeded= function(event){
+			var dbI=event.target.result;
+			var objectStore=dbI.createObjectStore("persons",{keyPath:"id"});
+			objectStore.createIndex("name","name",{unique:false});
+			objectStore.createIndex("email","email",{unique:true});
+			objectStore.transaction.oncomplete=function(event){
+				var customerObjectStore= dbI.transaction("persons","readwrite");
+				customerObjectStore.add({name:"daniel",email:"correo@correo.com"});
+			}
+
+		}
+	var db=this.initDb();
 	db.transaction(function(sql){
 		sql.executeSql("Create table if not exists videos (id unique, name)");
 		sql.executeSql('insert into videos values(1,"la maquina del tiempo ")');
@@ -11,17 +28,17 @@ function CreateDb(){
 		sql.executeSql('insert into videos values(7,"Xmen ")');
 		sql.executeSql('insert into videos values(8,"Un verano para recordar ")');
 	});
-	selectAll();
+	this.selectAll();
 }
-function selectAll(){
-	var db=initDb();
+ selectAll(){
+	var db=this.initDb();
 	db.transaction(function(query){
 		query.executeSql("select name from videos",[],function(query,result){
 			var filas=result.rows.length;
 			var write=document.getElementById("ulAllImagesBuscar");
 			var txt=document.getElementById("ulAllImages");
 			
-			for(i=0;i<filas;i++){
+			for(var i=0;i<filas;i++){
 			var li=document.createElement("li");
 			li.className="itemsAllImagenBuscar";
 			var p=document.createElement("p");
@@ -35,7 +52,7 @@ function selectAll(){
 			var li1=document.createElement("li");
 			var img=document.createElement("img");
 			var p1=document.createElement("p");
-			li1.className="itemsAllImage";
+			li1.className="itemsAllImagen";
 			img.className="imgAllItem";
 			p1.innerText=result.rows.item(i).name;
 			li1.appendChild(img);
@@ -47,7 +64,8 @@ function selectAll(){
 	});
 }
 
-function initDb(){
+ initDb(){
 	return window.openDatabase("mydb","1.0","db sql", 2 * 1024 * 1024);
 	
+}
 }
